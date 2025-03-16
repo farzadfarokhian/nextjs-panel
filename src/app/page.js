34b78685e181d696f2436c842c36258247d21b7f
farzad <farzad@iMac.local>
2025-03-16 06:13:48 +0330

    2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -13,13 +11,6 @@ export default function Home() {
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/panel"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
             ورود به پنل
           </a>
           <a
@@ -32,38 +23,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        
+      <footer className="">
       </footer>
     </div>
   );
--- src/app/panel/page.js
+++ src/app/panel/page.js
@@ -1,8 +1,88 @@
+"use client";
 
-export default function Page() {
-  return (
-    <div>
-T
-    </div>
-  );
+import { useState, useMemo, useCallback, useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { PencilIcon } from '@heroicons/react/24/solid';
+import {
+    ClientSideRowModelModule, ModuleRegistry,
+    QuickFilterModule,
+} from 'ag-grid-community';
+
+ModuleRegistry.registerModules([
+    QuickFilterModule,
+    ClientSideRowModelModule,
+]);
+
+
+export default function Home() {
+    const gridRef = useRef();
+
+    const onFilterTextBoxChanged = useCallback((e) => {
+        gridRef.current.api.setGridOption(
+            "quickFilterText",
+            e.target.value,
+        );
+    }, []);
+
+    console.log("gridRef", gridRef)
+
+    const gridStyle = useMemo(() => ({ width: "100%", textAlign: 'center', maxWidth: '63rem' }), []);
+
+    const [columnDefs] = useState([
+        {
+            headerName: 'ویرایش',
+            sortable: false,
+            cellRenderer: (params) => {
+                return (
+                    <button
+                        onClick={() => handleEdit(params)}
+                        className="h-full"
+                    >
+                        <PencilIcon className="h-5 w-5 hover:cursor-pointer" />
+                    </button>
+                );
+            }
+        },
+        { headerName: 'وضعیت', field: 'price' },
+        { headerName: 'تاریخ ثبت نام', field: 'price' },
+        { headerName: 'ایمل', field: 'model' },
+        { headerName: 'نام', field: 'make' }
+    ]);
+
+    const handleEdit = (params) => {
+        alert(`ویرایش: ${params.data.make} ${params.data.model}`);
+    };
+
+    const [rowData] = useState([
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxster', price: 72000 }
+    ]);
+
+
+    return (
+        <div className="flex flex-col items-center justify-center  ">
+            <div className="flex items-center justify-center  ">
+                <button className="px-6 py-3 hover:cursor-pointer bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-all duration-300">
+                    ایجاد جدید
+                </button>
+                <input
+                    type="text"
+                    id="filter-text-box"
+                    placeholder="جستجو ..."
+                    onInput={onFilterTextBoxChanged}
+                    className='p-3 m-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-300 shadow-sm hover:shadow-md'
+                />
+            </div>
+            <div style={gridStyle}>
+                <AgGridReact
+                    columnDefs={columnDefs}
+                    ref={gridRef}
+                    rowData={rowData}
+                    modules={[ClientSideRowModelModule]}
+                    domLayout="autoHeight"
+                />
+            </div>
+        </div>
+    );
 }
